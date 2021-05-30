a5a8c8d97105559fdac174aae7265a94dc511818
dashing-utility-e4d8 <eten.uket@yale.edu>
2021-05-30 17:28:36 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,10 @@
+def using_concat(array1, array2)
+  array1.concat(array2)
+end 
+
+def using_insert(array, new_num)
+  array = [1, 2, 3, 4, 5, 6, 7]
+  new_num = :Python
+  array.insert(new_num.to_s)
+  array
+end
