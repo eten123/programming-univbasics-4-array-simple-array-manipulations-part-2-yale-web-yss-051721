97ba90f6de50fcad45125a925a51767c7377cdd3
dashing-utility-e4d8 <eten.uket@yale.edu>
2021-05-30 17:28:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,9 +2,8 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end 
 
-def using_insert(array, new_num)
+def using_insert(array)
   array = [1, 2, 3, 4, 5, 6, 7]
   new_num = :Python
   array.insert(new_num.to_s)
-  array
 end
